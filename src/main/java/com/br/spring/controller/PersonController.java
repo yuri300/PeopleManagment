2003101f59a20f1420a6d5af46c5c2b06a03b490
yuri300 <yurisc50@gmail.com>
2021-12-17 21:47:36 -0300

    Developed read function list people

--- src/main/java/com/br/spring/controller/PersonController.java
+++ src/main/java/com/br/spring/controller/PersonController.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.br.spring.model.Person;
 import com.br.spring.repository.PersonRepository;
 
 @Controller
 public class PersonController {
-	
+
 	@Autowired
 	private PersonRepository personRepository;
 
@@ -18,9 +19,22 @@ public class PersonController {
 	public String begin() {
 		return "register/registerPerson";
 	}
+
 	@RequestMapping(method = RequestMethod.POST, value = "/savePerson")
-	public String savePerson(Person person) {
+	public ModelAndView savePerson(Person person) {
 		personRepository.save(person);
-		return "register/registerPerson";
+		
+		ModelAndView mv = new ModelAndView("register/registerPerson");
+		Iterable<Person> personIt = personRepository.findAll();
+		mv.addObject("people", personIt);
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value="/listPeople")
+	public ModelAndView People() {
+		ModelAndView mv = new ModelAndView("register/registerPerson");
+		Iterable<Person> personIt = personRepository.findAll();
+		mv.addObject("people", personIt);
+		return mv;
 	}
 }
